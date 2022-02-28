95a7e94b913d94559092d55495331ea1060d0795
Saiya <okarl@outlook.com>
2022-02-28 16:25:36 +0800

    feat: open-in-browser support ssh clone link

--- scripts/open-in-git-browser.sh
+++ scripts/open-in-git-browser.sh
@@ -3,7 +3,19 @@
 echo 'Open in Browser...'
 SHA=$1
 REPO_URL=$(git remote get-url origin)
-if [ -n $REPO_URL ]; then
-	URL=${REPO_URL%%.git}/commit/${SHA}
-	open ${URL}
+if [ -z $REPO_URL ]; then
+	exit
 fi
+
+# if is git ssh clone link
+if [[ $REPO_URL == "git@"* ]]; then
+	REPO_URL=${REPO_URL#git@}
+	REPO_URL=${REPO_URL/:/"/"}
+	REPO_URL="https://"$REPO_URL
+fi
+
+URL=${REPO_URL%%.git}/commit/${SHA}
+open ${URL}
+
+
+
