b5f946302cae577f8877b63beee09008033e254c
Saiya <okarl@outlook.com>
2024-07-10 23:34:39 +0800

    feat: support ssh and bitbucket repo url in open-in-git-browser

--- scripts/open-in-git-browser.sh
+++ scripts/open-in-git-browser.sh
@@ -7,6 +7,11 @@ if [ -z $REPO_URL ]; then
 	exit
 fi
 
+# if REPO_URL starts with ssh:// then remove it
+if [[ $REPO_URL == "ssh://"* ]]; then
+	REPO_URL=${REPO_URL#ssh://}
+fi
+
 # if is git ssh clone link
 if [[ $REPO_URL == "git@"* ]]; then
 	REPO_URL=${REPO_URL#git@}
@@ -14,8 +19,19 @@ if [[ $REPO_URL == "git@"* ]]; then
 	REPO_URL="https://"$REPO_URL
 fi
 
-URL=${REPO_URL%%.git}/commit/${SHA}
+# bitbucket related url
+if [[ $REPO_URL == *"bitbucket."* ]]; then
+	# get scheme, host and path from REPO_URL
+	SCHEME=${REPO_URL%%://*}
+	REPO_URL=${REPO_URL#*://}
+	HOST_NAME=${REPO_URL%%/*}
+	PATH_NAME=${REPO_URL#*/}
+	GROUP_NAME=${PATH_NAME%%/*}
+	REST_PATH=${PATH_NAME#*/}
+	REPO_URL=${SCHEME}://${HOST_NAME}/projects/${GROUP_NAME}/repos/${REST_PATH}
+	URL=${REPO_URL%%.git}/commits/${SHA}
+else
+	URL=${REPO_URL%%.git}/commit/${SHA}
+fi
+
 open ${URL}
-
-
-
